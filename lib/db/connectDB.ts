1529ac2fbc6797a1a8ebf94763e631f1c2507808
Nitoo kumari <neetunagdev258@example.com>
2026-09-05 04:19:06 +0500

    solved dbConnection error

--- lib/db/connectDB.ts
+++ lib/db/connectDB.ts
@@ -1,26 +1,27 @@
 import mongoose from "mongoose"
 
 type ConnectionObject = {
-    isConnected?:Number
+  isConnected?: number
 }
 
-const connection :ConnectionObject={
+const connection: ConnectionObject = {}
 
-}
-async function connectDB():Promise<void>{
-    if (connection.isConnected) {
-        console.log("Already connected to database")
-        return
-    }
-    try {
-       const db =  await mongoose.connect(process.env.MONGO_URI || "", {})
-       connection.isConnected = db.connections[0].readyState
-       console.log("DB connected successfully")
-    } catch (error) {
-        console.log("Database connection failed")
-        process.exit(1)
-        
-    }
+async function connectDB(): Promise<void> {
+  if (connection.isConnected === 1) {
+    console.log("Already connected to database")
+    return
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI || "")
+
+    connection.isConnected = db.connections[0].readyState
+
+    console.log("DB connected successfully")
+  } catch (error) {
+    console.error("Database connection failed:", error)
+    throw error
+  }
 }
 
 export default connectDB
