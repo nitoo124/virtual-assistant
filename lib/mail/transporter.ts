f5415f5c30d32e97284efbff4b6ec435af10e104
Nitoo kumari <neetunagdev258@example.com>
2026-09-05 03:23:58 +0500

    solved resend email issue

--- lib/mail/transporter.ts
+++ lib/mail/transporter.ts
@@ -1,9 +1,17 @@
 import nodemailer from "nodemailer"
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+})
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.error("SMTP ERROR:", error)
+    } else {
+        console.log("SMTP server is ready")
+    }
 })
